160c1d80c31a303a0f338d2dfebfd567043b80f1
boulakhf <boulakhf@PC-A105-06.iut-valence.fr>
2015-11-03 09:28:03 +0100

    Commit de fin de séance

--- src/fr/iutvalence/info/dut/m3105/gildedroseinn/refactoring/GildedRose.java
+++ src/fr/iutvalence/info/dut/m3105/gildedroseinn/refactoring/GildedRose.java
@@ -27,6 +27,12 @@ public class GildedRose
 	{	
 		item.setQuality((item.getQuality())-1);
 		item.setSellIn((item.getSellIn())-1);
+		if (item.getSellIn()<0){
+			item.setQuality((item.getQuality())-1);}
 	}
+
+	
+
+
 
 }
--- test/fr/iutvalence/info/dut/m3105/gildedroseinn/refactoring/GildedRoseTest.java
+++ test/fr/iutvalence/info/dut/m3105/gildedroseinn/refactoring/GildedRoseTest.java
@@ -8,16 +8,40 @@ import org.junit.Test;
 
 public class GildedRoseTest
 {
+	
+	
+	
 	@Test
 	public void itemsSellingAndQualityAreUpdatedEachDays()
 	{
-		 
-		 	Item item = new Item("+5 Dexterity Vest", 10, 20);
-			 int qualityBefore=item.getQuality();
-			 int sellInBefore=item.getSellIn();
-			 GildedRose.updateItem(item);
-			 assertEquals(qualityBefore,(item.getQuality())+1);
-			 assertEquals(sellInBefore,(item.getSellIn())+1);
+		 Item item = new Item("+5 Dexterity Vest", 10, 20);
+		 int qualityBefore=item.getQuality();
+		 int sellInBefore=item.getSellIn();
+		 GildedRose.updateItem(item);
+		 assertEquals(qualityBefore,(item.getQuality())+1);
+		 assertEquals(sellInBefore,(item.getSellIn())+1);
 	}
+	
+	@Test
+	public void itemsSellingAndQualityAreUpdatedWhenSellInIsUnderZero()
+	{
+		 
+		 Item item = new Item("+5 Dexterity Vest", 0, 20);
+		 int qualityBefore=item.getQuality();
+		 int sellInBefore=item.getSellIn();
+		 GildedRose.updateItem(item);
+		 assertEquals(qualityBefore,(item.getQuality())+2);
+		 assertEquals(sellInBefore,(item.getSellIn())+1);
+	}
+	
+	@Test
+	public void qualityNeverNegative(){
+		 Item item = new Item("+5 Dexterity Vest", 10, 15);
+		 int qualityBefore=item.getQuality();
+		 GildedRose.updateItem(item);
+		 //assertEquals(item.getQuality()+1,qualityBefore) || assertEquals(item.getQuality(),0);
+		 
+	}
+	
 
 }
