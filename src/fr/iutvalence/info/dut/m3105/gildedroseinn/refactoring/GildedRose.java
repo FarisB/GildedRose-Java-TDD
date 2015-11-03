54359568e2f182b649d34781c9869e02ff6a3329
boulakhf <boulakhf@PC-A105-06.iut-valence.fr>
2015-11-03 08:45:06 +0100

    First scenario about the update of all items each days tested

--- src/fr/iutvalence/info/dut/m3105/gildedroseinn/refactoring/GildedRose.java
+++ src/fr/iutvalence/info/dut/m3105/gildedroseinn/refactoring/GildedRose.java
@@ -25,6 +25,8 @@ public class GildedRose
 
 	public static void updateItem(Item item)
 	{	
+		item.setQuality((item.getQuality())-1);
+		item.setSellIn((item.getSellIn())-1);
 	}
 
 }
--- test/fr/iutvalence/info/dut/m3105/gildedroseinn/refactoring/GildedRoseTest.java
+++ test/fr/iutvalence/info/dut/m3105/gildedroseinn/refactoring/GildedRoseTest.java
@@ -2,14 +2,22 @@ package fr.iutvalence.info.dut.m3105.gildedroseinn.refactoring;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.ArrayList;
+
 import org.junit.Test;
 
 public class GildedRoseTest
 {
 	@Test
-	public void aSimpleBeginningTest()
+	public void itemsSellingAndQualityAreUpdatedEachDays()
 	{
-		assertEquals(true,false);
+		 
+		 	Item item = new Item("+5 Dexterity Vest", 10, 20);
+			 int qualityBefore=item.getQuality();
+			 int sellInBefore=item.getSellIn();
+			 GildedRose.updateItem(item);
+			 assertEquals(qualityBefore,(item.getQuality())+1);
+			 assertEquals(sellInBefore,(item.getSellIn())+1);
 	}
 
 }
